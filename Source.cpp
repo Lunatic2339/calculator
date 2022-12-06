2acfb77e509def2daa75035dd0b7820020a87bf8
Sunwoo Park <lunatic230139@gmail.com>
2022-12-06 21:40:42 +0900

    new commit

--- Source.cpp
+++ Source.cpp
@@ -6,9 +6,11 @@ using namespace std;
 int main()
 {
 	fraction fr1(1, 2, 2);
-	fraction fr2(2, -3, 2);
+	fraction fr2(2, -3, 3);
 	fraction fr3 = fr1 + fr2;
+	fraction fr4 = fr2 + fr1;
 	fr1.Print();
 	fr2.Print();
 	fr3.Print();
+	fr4.Print();
 }
