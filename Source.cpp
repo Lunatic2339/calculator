4938f27be3bbc655691a19bc3c882da8cbc9427a
Sunwoo Park <lunatic230139@gmail.com>
2022-12-08 19:08:25 +0900

    delete root

--- Source.cpp
+++ Source.cpp
@@ -1,20 +1,12 @@
 #include <iostream>
-#include "fraction.h"
 
 using namespace std;
 
 int main()
 {
-	fraction fr1(1, 2, 2);
-	fraction fr2(2, -3, 3);
-	fraction fr3 = fr1 + fr2;
-	fraction fr4 = fr2 + fr1;
-	fraction fr5 = fr1 + 3;
-	fraction fr6 = fr1 - fr2;
-	fr1.Print();
-	fr2.Print();
-	fr3.Print();
-	fr4.Print();
-	fr5.Print();
-	fr6.Print();
+	char a = 9;
+	int b = 10;
+	int c = a * b;
+
+	cout << c;
 }
--- fraction.cpp
+++ fraction.cpp
@@ -1,223 +1,35 @@
 #include "fraction.h"
 
-// fraction class default constructor
+int fraction::gcd(int a, int b)
+{
+	if (b == 0) return a;
+	else return gcd(b, a % b);
+}
+
+fraction::fraction(int n, int d, bool b)
+	: numerator(n), denominator(d)
+{
+}
+
 fraction::fraction()
+	: numerator(0), denominator(1)
 {
-	nArr.push_back(number(1,0,0));
 }
 
-//  fraction class custom constructor 1
-fraction::fraction(int denominator, int numerator)
+fraction::fraction(int n)
+	: numerator(n), denominator(1)
 {
-	nArr.push_back(number(denominator, numerator, 1));
 }
 
-//  fraction class custom constructor 2
-fraction::fraction(int denominator, int numerator, int root)
+fraction::fraction(int n, int d)
 {
-	nArr.push_back(number(denominator, numerator, root));
+	int x = gcd(n, d);
+	numerator = n / x;
+	denominator = d / x;
 }
 
-//  fraction class copy constructor
-fraction::fraction(const fraction& other)
+fraction fraction::operator+(const fraction& fr)
 {
-	nArr = other.nArr;
+	int x = gcd(this->numerator * fr.denominator + fr.numerator * this->denominator, this->denominator * fr.denominator);
+	return fraction((this->numerator * fr.denominator + fr.numerator * this->denominator)/x, (this->denominator * fr.denominator, true)/x);
 }
-
-
-
-// fraction class '+' operator overloading
-fraction fraction::operator+(const fraction& frac)
-{
-	fraction result = *this;
-	for (number n : frac.nArr)
-	{
-		int con = 0;
-		for (number& m : result.nArr)
-		{
-			if (n.root == m.root) m = m + n;
-			else ++con;
-		}
-		if (con == result.nArr.size()) result.nArr.push_back(n);
-	}
-	return result;
-}
-
-fraction fraction::operator+(int n)
-{
-	fraction result = *this;
-	number n1(1, n, 1);
-	int con = 0;
-	for (number& m : result.nArr)
-	{
-		if (m.root == 1) m = m + n1;
-		else ++con;
-	}
-	if (con == result.nArr.size()) result.nArr.push_back(n1);
-	return result;
-}
-
-fraction fraction::operator-(const fraction& frac)
-{
-	fraction result = *this;
-	for (number n : frac.nArr)
-	{
-		int con = 0;
-		for (number& m : result.nArr)
-		{
-			if (n.root == m.root) m = m - n;
-			else ++con;
-		}
-		if (con == result.nArr.size()) result.nArr.push_back(number(1,0,n.root) - n);
-	}
-	return result;
-}
-
-fraction fraction::operator-(int n)
-{
-	fraction result = *this;
-	number n1(1, -n, 1);
-	int con = 0;
-	for (number& m : result.nArr)
-	{
-		if (m.root == 1) m = m + n1;
-		else ++con;
-	}
-	if (con == result.nArr.size()) result.nArr.push_back(n1);
-	return result;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// number inner class default constrctor
-fraction::number::number()
-	: denom(0), num(0), root(0)
-{
-	sign = 1;
-}
-
-// number inner class custom constrctor
-fraction::number::number(int x, int y, unsigned int z)
-	: denom(abs_val(x)), num(abs_val(y)), root(z)
-{
-	if (x * y >= 0) sign = 1;
-	else sign = -1;
-}
-
-// number inner class '+' operator overloading
-fraction::number fraction::number::operator+(number n)
-{
-	if (this->root == n.root)
-	{
-		if (this->sign == 1 && n.sign == 1)
-		{
-			int x = gcd(this->denom * n.denom, this->num * n.denom + n.num * this->denom);
-			number result((this->denom * n.denom) / x, (this->num * n.denom + n.num * this->denom) / x, this->root);
-			return result;
-		}
-		else if (this->sign == 1 && n.sign == -1)
-		{
-			int x = gcd(this->denom * n.denom, this->num * n.denom - n.num * this->denom);
-			number result((this->denom * n.denom) / x, (this->num * n.denom - n.num * this->denom) / x, this->root);
-			return result;
-		}
-		else if (this->sign == -1 && n.sign == 1)
-		{
-			int x = gcd(this->denom * n.denom, n.num * this->denom - this->num * n.denom);
-			number result((this->denom * n.denom) / x, (n.num * this->denom - this->num * n.denom) / x, this->root);
-			return result;
-		}
-		else if (this->sign == -1 && n.sign == -1)
-		{
-			int x = gcd(this->denom * n.denom, -this->num * n.denom - n.num * this->denom);
-			number result((this->denom * n.denom) / x, (-this->num * n.denom - n.num * this->denom) / x, this->root);
-			return result;
-		}
-	}
-
-	else throw "not same root value";
-}
-
-
-fraction::number fraction::number::operator-(number n)
-{
-	if (this->root == n.root)
-	{
-		if (this->sign == 1 && n.sign == 1)
-		{
-			int x = gcd(this->denom * n.denom, this->num * n.denom - n.num * this->denom);
-			number result((this->denom * n.denom) / x, (this->num * n.denom - n.num * this->denom) / x, this->root);
-			return result;
-		}
-		else if (this->sign == 1 && n.sign == -1)
-		{
-			int x = gcd(this->denom * n.denom, this->num * n.denom + n.num * this->denom);
-			number result((this->denom * n.denom) / x, (this->num * n.denom + n.num * this->denom) / x, this->root);
-			return result;
-		}
-		else if (this->sign == -1 && n.sign == 1)
-		{
-			int x = gcd(this->denom * n.denom, - n.num * this->denom - this->num * n.denom);
-			number result((this->denom * n.denom) / x, (- n.num * this->denom - this->num * n.denom) / x, this->root);
-			return result;
-		}
-		else if (this->sign == -1 && n.sign == -1)
-		{
-			int x = gcd(this->denom * n.denom, - this->num * n.denom + n.num * this->denom);
-			number result((this->denom * n.denom) / x, (-this->num * n.denom + n.num * this->denom) / x, this->root);
-			return result;
-		}
-	}
-
-	else throw "not same root value";
-}
-
-fraction::number fraction::number::operator*(number n)
-{
-	std::vector<int> factor = intFactor(this->root * n.root);
-	std::vector<int> factorcheck;
-	factorcheck.push_back(factor.at(0));
-	for (int i = 1; i < factor.size(); ++i)
-	{
-		int duple = 0;
-		for (int j : factorcheck)
-		{
-			if (i == j) 
-			{
-				n.num = n.num * factor.at(i);
-				factor.erase(factor.begin() + i);
-				--i;
-			}
-		}
-		if (duple == factorcheck.size()) factorcheck.push_back(i);
-	}
-	int newroot = 1;
-	for (int i : factor) newroot = newroot * i;
-
-	int x = gcd(this->denom * n.denom, this->num * n.num);
-	number result((this->denom * n.denom) / x, (this->num * n.num) / x, newroot);
-	result.sign = this->sign * n.sign;
-	
-	return result;
-}
-
-
